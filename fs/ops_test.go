e47aa70054817561d08c7e03f19dd18509e31d3f
Vincent Demeester <vincent@sbr.pm>
2018-07-04 09:24:43 +0200

    Handle symlinks in `fs.FromDir`
    For any symlinks, it will *re-create* them the same way they were in
    the source file instead of trying to read them and copy them as file.
    
    This should fix errors when doing a `fs.FromDir` that contains dead link.
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- fs/ops_test.go
+++ fs/ops_test.go
@@ -21,3 +21,22 @@ func TestFromDir(t *testing.T) {
 
 	assert.Assert(t, fs.Equal(dir.Path(), expected))
 }
+
+func TestFromDirSymlink(t *testing.T) {
+	dir := fs.NewDir(t, "test-from-dir", fs.FromDir("testdata/copy-test-with-symlink"))
+	defer dir.Remove()
+
+	expected := fs.Expected(t,
+		fs.WithFile("1", "1\n"),
+		fs.WithDir("a",
+			fs.WithFile("1", "1\n"),
+			fs.WithFile("2", "2\n"),
+			fs.WithDir("b",
+				fs.WithFile("1", "1\n"),
+				fs.WithSymlink("2", "../2"),
+				fs.WithSymlink("3", "/some/inexistent/link"),
+				fs.WithSymlink("4", "5"),
+			)))
+
+	assert.Assert(t, fs.Equal(dir.Path(), expected))
+}
