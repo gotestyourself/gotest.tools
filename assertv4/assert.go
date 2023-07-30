7077bf0dbbfbc345a127e5e1791ade839d821558
Daniel Nephin <dnephin@gmail.com>
2023-07-29 23:21:59 -0400

    Interface

--- assertv4/assert.go
+++ assertv4/assert.go
@@ -45,11 +45,11 @@ type TestingT interface {
 //	v, ok := settings["max"]
 //	assert.True(t, ok, settings)
 //	// main_test.go:32: We wanted settings to have the key "max", but it did not.
-//	// settings={"min": 34}.
+//	// settings=map[min: 34].
 //
 // Fail with t.Fail instead of t.FailNow
 //
-//	assert.True(assert.AndContinue(t),
+//	assert.True(assert.AndContinue(t), false)
 func True(t TestingT, expr bool, values ...any) bool {
 	if expr == true {
 		return true
@@ -61,27 +61,62 @@ func True(t TestingT, expr bool, values ...any) bool {
 // Nil fails the test with [t.FailNow] if the value of err is not nil.
 // Nil uses the Go source and comments in the test function to print a
 // helpful failure message.
+//
+//	err := os.RemoveAll(dir)
+//	assert.Nil(t, err)
+//	// main_Test.go:52: We wanted os.RemoveAll to not error, but it returned
+//	// and error of type os.PathError with value: "permission denied"
 func Nil(t TestingT, err error) bool {
 	if err == nil {
 		return true
 	}
 	t.Helper()
+	// TODO:
+	t.Logf("Expression was False")
+	t.FailNow()
 	return false
 }
 
-func Empty(t TestingT, v string) bool {
-	if v == "" {
+type Sequence[T any, K comparable] interface {
+	~string | ~[]T | ~map[K]T | ~chan T
+}
+
+// Empty fails the test with [t.FailNow] if the value of v is not the empty
+// string. Empty is particularly useful with [cmp.Diff].
+// Empty uses the Go source and comments in the test function to print a
+// helpful failure message.
+//
+// Deep equal structs
+//
+//	want := Response{...}
+//	got := DoRPC(method, arg1)
+//	assert.Empty(t, cmp.Diff(want, got))
+//	// main_test.go:14: DoRPC(method, arg1) diff (-want +got):
+//	// ...(a very nice diff)
+func Empty[S Sequence[T, K], T any, K comparable](t TestingT, v S) bool {
+	if len(v) == 0 {
 		return true
 	}
 	t.Helper()
+	// TODO:
+	t.Logf("Expression was false")
+	t.FailNow()
 	return false
 }
 
+// Equal fails the test with [t.FailNow] if x is not equal to y.
+// Equal uses the Go source and comments in the test function to print a
+// helpful failure message.
+//
+// When the -update flag is set, Equal will update the value of the want
+// (or expected) variable to the value of the got (or actual) variable.
 func Equal(t TestingT, x any, y any) bool {
 	if x == y {
 		return true
 	}
 	t.Helper()
+	t.Logf("Expression was false")
+	t.FailNow()
 	return false
 }
 
